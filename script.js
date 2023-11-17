9d050230feaa2d0ed74100b71e9120c926875f8a
Gabriel Esteves <131073734+geaduque@users.noreply.github.com>
2023-11-17 02:28:09 -0300

    Atualizando o script

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// var dragFranchiseName = [
-//   "Drag Race US",
-//   "Drag Race UK",
-//   "Canada's Drag Race",
-//   "Drag Race Down Under",
-// ];
-
 var dragFranchiseImage = [
   "https://m.media-amazon.com/images/M/MV5BODJkZjk3MzgtNzhhNy00ZmFhLTkxNzktZGYzOWU4Y2E1MjZkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
   "https://m.media-amazon.com/images/M/MV5BZTg2ZWUyNTEtYTliYy00ZDUzLWFlMGQtYjU3YjZlNzEwNGRhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
@@ -15,7 +8,7 @@ var dragFranchiseImage = [
 var renderArrayElements = document.getElementById("array");
 
 for (var i = 0; i < dragFranchiseImage.length; i++) {
-  var imgElement = document.createElement("img"); // Cria um novo elemento <img>
-  imgElement.src = dragFranchiseImage[i]; // Define o atributo 'src' do elemento <img> com o URL da imagem no índice 'i' do array
-  renderArrayElements.appendChild(imgElement); // Adiciona o elemento <img> ao elemento com o ID 'array' no DOM
+  var imgElement = document.createElement("img");
+  imgElement.src = dragFranchiseImage[i]; 
+  renderArrayElements.appendChild(imgElement);
 }
